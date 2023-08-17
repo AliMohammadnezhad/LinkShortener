976878dd446eff3e7009ac06b5469fa5aabca99d
Ali <alimohammadnejad271@gmail.com>
2023-08-17 11:00:47 +0330

    Added Link domain model and updated project file for Shortener functionality

---
+++ Shortener/Models/Domain/Link.cs
@@ -0,0 +1,19 @@
+﻿namespace Shortener.Models.Domain;
+
+public sealed class Link
+{
+    public const string TableName = "Links";
+
+    public long Id { get; set; }
+    public string ShortCode { get; set; }
+    public string LongUrl { get; set; }
+
+    public Link(string shortCode, string longUrl)
+    {
+        ShortCode = shortCode;
+        LongUrl = longUrl;
+    }
+
+    public static Link Create(string shortCode, string longUrl)
+        => new(shortCode, longUrl);
+}
