fa9872566ba25663f858c04e9d055ff9060a6c5b
Ali <alimohammadnejad271@gmail.com>
2023-08-17 11:12:11 +0330

    Implemented data interfaces and context for Shortener functionality

---
+++ Shortener/Data/ILinkRepository.cs
@@ -0,0 +1,12 @@
+﻿using Shortener.Models.Domain;
+
+namespace Shortener.Data;
+
+public interface ILinkRepository
+{
+    Task AddAsync(Link link, CancellationToken cancellationToken);
+    Task<(bool found, string? value)> TryGetLongUrlAsync(string shortCode, CancellationToken cancellationToken);
+    Task<(bool found, string? value)> TryGetShortUrlAsync(string longUrl, CancellationToken cancellationToken);
+    Task<bool> SaveChangesAsync(CancellationToken cancellationToken);
+
+}
---
+++ Shortener/Data/ShortenerDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shortener.Models.Domain;
+
+namespace Shortener.Data;
+
+public class ShortenerDbContext : DbContext
+{
+    public const string DefaultSchema = "shortener";
+
+    public ShortenerDbContext(DbContextOptions<ShortenerDbContext> dbContextOptions)
+        : base(dbContextOptions)
+    {
+
+    }
+
+    public DbSet<Link> Links => Set<Link>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    { 
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Link>(link =>
+        {
+            link.ToTable(Link.TableName, ShortenerDbContext.DefaultSchema);
+            link.HasKey(x => x.Id);
+
+            link.Property(x => x.ShortCode)
+                .IsRequired();
+
+            link.Property(x => x.LongUrl)
+                .IsRequired();
+
+            link.HasIndex(x => x.ShortCode)
+                    .IsUnique(true);
+
+            link.HasIndex(x => x.LongUrl)
+                    .IsUnique(true);
+        });
+    }
+}
