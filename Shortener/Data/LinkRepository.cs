4e78d42692c50a24485961a7353ca2ff3c2e7c14
Ali <alimohammadnejad271@gmail.com>
2023-08-17 11:16:58 +0330

    Added LinkRepository implementation for data management in Shortener

---
+++ Shortener/Data/LinkRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shortener.Models.Domain;
+
+namespace Shortener.Data;
+
+public class LinkRepository : ILinkRepository
+{
+    private readonly ShortenerDbContext _shortenerDbContext;
+
+    public LinkRepository(ShortenerDbContext shortenerDbContext)
+        => _shortenerDbContext = shortenerDbContext;
+
+    public async Task AddAsync(Link link, CancellationToken cancellationToken)
+        => await _shortenerDbContext.Links.AddAsync(link, cancellationToken);
+
+    public async Task<(bool found, string? value)> TryGetLongUrlAsync(string shortCode, CancellationToken cancellationToken)
+    {
+        var link = await _shortenerDbContext.Links.FirstOrDefaultAsync(x => x.ShortCode == shortCode, cancellationToken);
+
+        if (link != null)
+        {
+            return (true, link.LongUrl);
+        }
+
+        return (false, null);
+    }
+
+    public async Task<(bool found, string? value)> TryGetShortUrlAsync(string longUrl, CancellationToken cancellationToken)
+    {
+        var link = await _shortenerDbContext.Links.FirstOrDefaultAsync(x => x.LongUrl == longUrl, cancellationToken);
+
+        if (link != null)
+        {
+            return (true, link.ShortCode);
+        }
+
+        return (false, null);
+    }
+     
+    public async Task<bool> SaveChangesAsync(CancellationToken cancellationToken)
+        => await _shortenerDbContext.SaveChangesAsync(cancellationToken) > 0;
+
+
+}
